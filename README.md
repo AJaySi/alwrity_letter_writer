191462d3969581e15b00b573661319ded215d718
ajaysi <ajay.calsoft@gmail.com>
2024-05-03 12:51:07 +0530

    Alwrity AI Business Letter writer

---
+++ README.md
@@ -0,0 +1,30 @@
+AI Email Writer with Streamlit
+
+This project aims to create a user-friendly web application using Streamlit that assists users in composing effective emails for various purposes and scenarios. By leveraging the power of AI, the application provides a convenient and efficient way to overcome writer's block, generate ideas, and craft professional and personalized emails.
+
+Note: Required Google Gemini API keys.
+
+### Features
+
+- Multiple Email Categories: Choose from Professional, Business, Personal, or Creative emails.
+
+- Specific Email Types: Select from a variety of common email types within each category (e.g., Job Application, Meeting Request, Sales Pitch, Thank You Note).
+
+- Tailored Input Fields: Provide specific details relevant to the chosen email type.
+
+- AI-Powered Email Generation: Integrate your AI model to generate email content based on user inputs and selected email type.
+
+- Customizable Tone and Style: Specify the desired tone and style of the email (e.g., Formal, Informal, Persuasive).
+
+- Optional Keywords and Call to Action: Include specific keywords or phrases and a clear call to action in the generated email.
+
+### Getting Started
+Clone the repository:
+git clone https://github.com/your-username/ai-email-writer.git
+Install dependencies:
+pip install streamlit
+
+### Install any additional libraries required for your AI model and email sending functionality
+Run the application:
+streamlit run app.py
+Access the application: Open your web browser and navigate to the URL provided by Streamlit (usually http://localhost:8501).
---
+++ alwrity_business_letter_writer.py
@@ -0,0 +1,168 @@
+import time #Iwish
+import os
+import json
+import requests
+import streamlit as st
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
+import google.generativeai as genai
+
+
+def main():
+    # Set page configuration
+    st.set_page_config(
+        page_title="Alwrity - AI Business Letter Writer (Beta)",
+        layout="wide",
+    )
+    # Remove the extra spaces from margin top.
+    st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 0rem;
+                    padding-bottom: 0rem;
+                    padding-left: 1rem;
+                    padding-right: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+    st.markdown(f"""
+      <style>
+      [class="st-emotion-cache-7ym5gk ef3psqc12"]{{
+            display: inline-block;
+            padding: 5px 20px;
+            background-color: #4681f4;
+            color: #FBFFFF;
+            width: 300px;
+            height: 35px;
+            text-align: center;
+            text-decoration: none;
+            font-size: 16px;
+            border-radius: 8px;’
+      }}
+      </style>
+    """
+    , unsafe_allow_html=True)
+
+    # Hide top header line
+    hide_decoration_bar_style = '<style>header {visibility: hidden;}</style>'
+    st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
+    # Hide footer
+    hide_streamlit_footer = '<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;}</style>'
+    st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
+
+    # Title and description
+    st.title("✍️ Alwrity - AI Business Letter Writer (Beta)")
+
+
+    with st.expander("**PRO-TIP** - Read the instructions below.", expanded=True):
+        col1, col2, space = st.columns([5, 5, 0.5])
+        with col1:
+            # Letter Type Selection
+            letter_types = ["Proposal", "Complaint", "Thank You", "Reference Letter", "Resignation Letter"]
+            letter_type = st.selectbox("**Select Letter Type**", letter_types)
+            key_points = st.text_area("**Key Points to Include in the letter**")
+        with col2:
+            # Recipient Information
+            recipient_name = st.text_input("**Enter Recipient's Name**")
+            # Sender Information
+            #sender_name = st.text_input("**Enter Sender Name**")
+
+    if st.button('**Write Business Letter**'):
+        with st.status("Assigning AI professional to write your letter..", expanded=True) as status:
+            if not key_points or not recipient_name:
+                st.error("🚫 Error: Enter all the details, least you can do..")
+            else:
+                response = business_letter_writer(letter_type, key_points, recipient_name, status)
+                if response:
+                    st.subheader(f'**🧕🔬👩 Alwrity can make mistakes. Your Final Business Letter!**')
+                    st.write(response)
+                else:
+                    st.error("💥**Failed to write Letter. Please try again!**")
+
+
+def business_letter_writer(letter_type, key_points, recipient_name, status):
+    """ Email project_update_writer """
+
+    prompt = f"""
+        Letter Type: {letter_type}
+        Recipient Name: {recipient_name}
+
+        Letter Content/Purpose:  {key_points} 
+
+        Instructions for AI:
+
+        * Generate a professional business letter based on the specified letter type and content/purpose.
+        * Infer the tone and desired outcome based on the letter type and content.
+        * Include relevant details and information commonly found in such letters. 
+        * Maintain a clear and concise writing style.
+    """
+    status.update(label="Writing Business Letter...")
+    try:
+        response = generate_text_with_exception_handling(prompt)
+        return response
+    except Exception as err:
+        st.error(f"Exit: Failed to get response from LLM: {err}")
+        exit(1)
+
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def generate_text_with_exception_handling(prompt):
+    """
+    Generates text using the Gemini model with exception handling.
+
+    Args:
+        api_key (str): Your Google Generative AI API key.
+        prompt (str): The prompt for text generation.
+
+    Returns:
+        str: The generated text.
+    """
+
+    try:
+        genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+
+        generation_config = {
+            "temperature": 1,
+            "top_p": 0.95,
+            "top_k": 0,
+            "max_output_tokens": 8192,
+        }
+
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+        ]
+
+        model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                                      generation_config=generation_config,
+                                      safety_settings=safety_settings)
+
+        convo = model.start_chat(history=[])
+        convo.send_message(prompt)
+        return convo.last.text
+
+    except Exception as e:
+        st.exception(f"An unexpected error occurred: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    main()
