1ae324e1a116dd49ca67c220caa622813d5ce3b4
ajaysi <ajay.calsoft@gmail.com>
2024-06-30 12:23:10 +0530

    button CSS

--- alwrity_business_letter_writer.py
+++ alwrity_business_letter_writer.py
@@ -26,35 +26,29 @@ def main():
                     padding-left: 1rem;
                     padding-right: 1rem;
                 }
+                 div.stButton > button:first-child {
+            background: #1565C0;
+            color: white;
+            border: none;
+            padding: 12px 24px;
+            border-radius: 8px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 10px 2px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+            font-weight: bold;
+        }
+
+        div.stButton > button:hover:first-child {
+            background-color: #1976A2;
+            box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.3);
+        }
         </style>
         """, unsafe_allow_html=True)
-    st.markdown(f"""
-      <style>
-      /* Custom button styling */
-		div.stButton > button:first-child {
-		    background: #1565C0;
-		    color: white;
-		    border: none;
-		    padding: 12px 24px;
-		    border-radius: 8px;
-		    text-align: center;
-		    text-decoration: none;
-		    display: inline-block;
-		    font-size: 16px;
-		    margin: 10px 2px;
-		    cursor: pointer;
-		    transition: background-color 0.3s ease;
-		    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
-		    font-weight: bold;
-		}
-		
-		div.stButton > button:hover:first-child {
-		    background-color: #1976A2;
-		    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.3);
-		}
-      </style>
-    """
-    , unsafe_allow_html=True)
 
     # Hide top header line
     hide_decoration_bar_style = '<style>header {visibility: hidden;}</style>'
